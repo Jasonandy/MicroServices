22fd468c8bfa7d4b2b58444368828fc9368a03f9
Gooogle <jasonandy@hotmail.com>
2018-02-17 22:41:17 +0800

    Happy New Year~ 2018

---
+++ microservice-api/src/cn/ucaner/microservice/api/utils/Utils.java
@@ -0,0 +1,35 @@
+/**
+ * <html>
+ * <body>
+ *  <P> Copyright 1994 JasonInternational</p>
+ *  <p> All rights reserved.</p>
+ *  <p> Created on 2018年2月17日 </p>
+ *  <p> Created by Jason</p>
+ *  </body>
+ * </html>
+ */
+package cn.ucaner.microservice.api.utils;
+
+/**     
+ * @Package：cn.ucaner.microservice.api.utils   
+ * @ClassName：Utils   
+ * @Description：   <p> 通用工具类</p>
+ * @Author： - Jason   
+ * @CreatTime：2018年2月17日 下午10:38:01   
+ * @Modify By：   
+ * @ModifyTime：  2018年2月17日
+ * @Modify marker：   
+ * @version    V1.0
+ */
+public class Utils {
+	
+	/**
+	 * @Description: 大年初二假装学习
+	 * @Autor: Jason - Jasonandy@hotmail.com
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Happy new Year 2018 !");
+	}
+
+}
